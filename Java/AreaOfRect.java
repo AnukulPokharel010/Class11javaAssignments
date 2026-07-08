b5e9abe0948ae904b80ed5d82f00564ca0325730
Anukul Pokharel <anukulpok@gmail.com>
2026-07-08 12:26:42 +0545

    Add files via upload

---
+++ Java/AreaOfRect.java
@@ -0,0 +1,8 @@
+public class AreaOfRect{
+ public static void main(String args[]){
+    int l=30;
+    int b=25;
+    int Area=l*b;
+    System.out.println("Area Of Rectangle:"+Area);
+ }
+}
---
+++ Java/PerimeterOfRect.java
@@ -0,0 +1,8 @@
+public class PerimeterOfRect {
+    public static void main(String[] args) {
+        int l=15;
+        int b=25;
+        int perimeter=2*(l+b);
+        System.out.println("Perimeter of Rectangle:"+perimeter);
+    }
+}
